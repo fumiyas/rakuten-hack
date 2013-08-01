867311fd31a002e3d67fe920c552ef48d9935ff2
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-08-02 00:09:49 +0900

    uncheck-mailmagazinecheckboxes: Disable <checkbox id="mail(Nws|Mgz)\d" ...> too

--- rakuten-uncheck-mailmagazinecheckboxes.user.js
+++ rakuten-uncheck-mailmagazinecheckboxes.user.js
@@ -12,7 +12,7 @@
 //               <http://www.OSSTech.co.jp/>
 //
 // License: GNU General Public License version 2
-// Date: 2013-07-24, since 2007-01-06
+// Date: 2013-08-02, since 2007-01-06
 
 (function() {
   var RakutenUncheckMailMagazinesCheckBoxes =
@@ -22,11 +22,12 @@
       var input = document.getElementsByTagName( "input" );
       for (var i = 0; i < input.length; i++) {
 	name = input[i].getAttribute("name");
+	id = input[i].getAttribute("id");
 	type = input[i].getAttribute("type");
 	if (!name || !type || type != "checkbox") {
 	  continue;
 	}
-	if (name == "rmail_check" || name == "shop_rating_check" || name == "newsId" || name == "bookmark_check" || name.lastIndexOf("f_rmail_", 0) == 0) {
+	if (name == "rmail_check" || name == "shop_rating_check" || name == "newsId" || id.lastIndexOf("mailMgz", 0) == 0 || id.lastIndexOf("mailNws", 0) == 0 || name == "bookmark_check") {
 	  //input[i].setAttribute("checked", false);
 	  input[i].checked = false;
 	}
