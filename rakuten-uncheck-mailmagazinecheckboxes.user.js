9d4ea679074194f06cff9bbc507d5a34bb3f796f
SATOH Fumiyasu <fumiyas@osstech.co.jp>
2013-09-28 02:32:12 +0900

    Add hack for https://*.checkout.rakuten.co.jp/*

--- rakuten-uncheck-mailmagazinecheckboxes.user.js
+++ rakuten-uncheck-mailmagazinecheckboxes.user.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name            Rakuten: Uncheck "want mail magazines" checkboxes by default
-// @namespace       http://www.sfo.jp/
+// @namespace       http://fumiyas.github.io/
 // @description     Uncheck "want mail magazines" checkboxes in Rakuten ordering page
 // @include         https://*.step.rakuten.co.jp/*
 // @include         https://*.travel.rakuten.co.jp/*
+// @include         https://*.checkout.rakuten.co.jp/*
 // @grant	    none
 // ==/UserScript==
 
 // Copyright (c) 2007-2013 SATOH Fumiyasu @ OSS Technology Corp., Japan
-//               <https://GitHub.com/fumiyas/>
+//               <https://GitHub.com/fumiyas/rakuten-hack>
 //               <http://www.OSSTech.co.jp/>
 //
 // License: GNU General Public License version 2
-// Date: 2013-08-02, since 2007-01-06
 
 (function() {
   var RakutenUncheckMailMagazinesCheckBoxes =
@@ -27,7 +27,14 @@
 	if (!name || !type || type != "checkbox") {
 	  continue;
 	}
-	if (name == "rmail_check" || name == "shop_rating_check" || name == "newsId" || id.lastIndexOf("mailMgz", 0) == 0 || id.lastIndexOf("mailNws", 0) == 0 || name == "bookmark_check") {
+	if (name == "rmail_check" ||
+	    name == "shop_rating_check" ||
+	    name == "newsId" ||
+	    name == "shopMail" ||
+	    name == "coutMail" ||
+	    id.lastIndexOf("mailMgz", 0) == 0 ||
+	    id.lastIndexOf("mailNws", 0) == 0 ||
+	    name == "bookmark_check") {
 	  //input[i].setAttribute("checked", false);
 	  input[i].checked = false;
 	}
